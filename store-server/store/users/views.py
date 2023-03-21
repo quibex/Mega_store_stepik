3b4f6dd858373c65dfe1de37e91db01c50305ea1
EAbdisalamov <eabdisalamov@mai.education>
2023-03-21 22:48:45 +0300

    Successful end

--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -45,9 +45,12 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store profile',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user) }
+
+    context = {
+        'title': 'Store profile',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'users/profile.html', context)
 
 def logout(request):
