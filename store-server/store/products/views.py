8b7377f8fc474663fb4f2cd7994746355fd573ae
EAbdisalamov <eabdisalamov@mai.education>
2023-03-23 10:23:51 +0300

    End 5 module

--- store-server/store/products/views.py
+++ store-server/store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import Product, ProductCategory, Basket
 from users.models import User
@@ -19,7 +20,7 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
-
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -33,6 +34,7 @@ def basket_add(request, product_id):
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
